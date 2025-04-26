569ce6891a86402f16431487657f0eb9c6b1a2b8
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-04-26 12:38:44 -0500

    Ummmmm idk

--- assets/scripts/editor_plugin.gd
+++ assets/scripts/editor_plugin.gd
@@ -1,6 +1,6 @@
 @tool extends EditorPlugin
 
-var PREVIEW_GENERATOR_SCRIPT := preload("res://addons/penny_atlas_godot/assets/scripts/composite_texture2d_preview_generator.gd")
+var PREVIEW_GENERATOR_SCRIPT := preload("res://addons/fatlas/assets/scripts/composite_texture2d_preview_generator.gd")
 var preview_generator : EditorResourcePreviewGenerator
 
 func _enable_plugin() -> void:
@@ -13,25 +13,6 @@ func _enable_plugin() -> void:
 func _disable_plugin() -> void:
 	EditorInterface.get_resource_previewer().remove_preview_generator(preview_generator)
 	EditorInterface.get_resource_filesystem().resources_reimported.disconnect(_on_resources_reimported)
-
-
-func _has_main_screen() -> bool:
-	# return true
-	return false
-
-
-func _make_visible(visible: bool) -> void:
-	# if main_panel:
-	# 	main_panel.visible = visible
-	pass
-
-
-func _get_plugin_name() -> String:
-	return "Atlas"
-
-
-func _get_plugin_icon() -> Texture2D:
-	return EditorInterface.get_editor_theme().get_icon("AtlasTexture", "EditorIcons")
 
 
 func _on_resources_reimported(resources: PackedStringArray) -> void:
