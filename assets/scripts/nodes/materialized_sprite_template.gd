607848dc69efe2b1e2df2b7a5dd23651fa7bf8e8
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-06-08 10:30:18 -0500

    Fixup

--- assets/scripts/nodes/materialized_sprite_template.gd
+++ assets/scripts/nodes/materialized_sprite_template.gd
@@ -30,7 +30,7 @@ var _preview_component : MaterializedSpriteComponent.TextureComponent
 		preview.refresh()
 		_refresh_preview_visibility_here()
 func _refresh_preview_visibility_here() -> void:
-	# preview.visible = Engine.is_editor_hint()
+	# preview.visible = preview.mirrored or preview.component != MaterializedSpriteComponent.TextureComponent.ALBEDO
 	preview.visible = not Engine.is_editor_hint() or preview.mirrored or preview.component != MaterializedSpriteComponent.TextureComponent.ALBEDO
 
 
